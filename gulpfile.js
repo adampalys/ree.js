bc5d1d35f0f54ddb5fff993a5c5ab3830e61afa4
Aki Rodic <aleksandar.xyz@gmail.com>
2015-11-25 21:00:12 +0900

    Added demo application (editor)

--- gulpfile.js
+++ gulpfile.js
@@ -13,41 +13,45 @@
 
   // Clean output directory
   gulp.task('clean', function (cb) {
-    del(['dist', 'src/ree.vulcanized.html'], cb);
-  });
-
-  // Clean output directory
-  gulp.task('clean-vulcanized', function (cb) {
-    del(['src/ree.vulcanized.html'], cb);
+    del(['dist', '.temp'], cb);
   });
 
   gulp.task('copy', function (cb) {
-    gulp.src(['src/ree.html'])
-    .pipe($.rename('ree.vulcanized.html'))
-    .pipe(gulp.dest('src'))
+    gulp.src(['../../bower_components/**/*'])
+    .pipe(gulp.dest('.temp'))
+    .on('end', function(){
+      gulp.src(['src/**/*'])
+      .pipe(gulp.dest('.temp/ree.js/src'))
+      .on('end', cb);
+    });
+  });
+
+  gulp.task('copy-demo', function (cb) {
+    gulp.src(['src/demo/**/*'])
+    .pipe(gulp.dest('dist/demo'))
     .on('end', cb);
   });
 
   // Vulcanize granular configuration
   gulp.task('vulcanize', function () {
-    var DEST_DIR = 'dist';
-    return gulp.src('src/ree.vulcanized.html')
-      .pipe($.vulcanize({
-        stripComments: true,
-        inlineCss: true,
-        inlineScripts: true
-      }))
-      .on('error', function(e) {
-        console.log(e);
-      })
-      .pipe($.rename('ree.html'))
-      .pipe($.minifyInline())
-      .pipe($.crisper({
-        scriptInHead: false, // true is default
-        onlySplit: false
-      }))
-      .pipe(gulp.dest(DEST_DIR))
-      .pipe($.size({title: 'vulcanize'}));
+    return gulp.src([
+      '.temp/ree.js/src/ree-app.html'
+    ])
+    .pipe($.vulcanize({
+      stripComments: true,
+      inlineCss: true,
+      inlineScripts: true
+    }))
+    .on('error', function(e) {
+      console.log(e);
+    })
+    .pipe($.minifyInline())
+    .pipe($.crisper({
+      scriptInHead: false,
+      onlySplit: false
+    }))
+    .pipe(gulp.dest('dist'))
+    .pipe($.size({title: 'vulcanize'}));
   });
 
   // Commit to dist branch
@@ -55,13 +59,11 @@
     return gulp.src([
       'dist/**/*',
       'README.md',
-      'LICENSE',
-      '.bowerrc',
-      'bower.json',
-      'package.json'
+      'LICENSE'
     ])
     .pipe($.ghPages({ branch: 'dist', force: true }));
   });
+
   // Lint JavaScript
   gulp.task('lint', function() {
     return gulp.src([
@@ -86,9 +88,9 @@
 
     gulp.watch([
       'src/**/*.js',
-      'src/**/*.html',
-      '!src/ree.vulcanized.html'],
-    ['build']);
+      'src/**/*.html'],
+      ['build']
+    );
 
     runSequence('build');
 
@@ -100,7 +102,7 @@
       'clean',
       'copy',
       'vulcanize',
-      'clean-vulcanized',
+      'copy-demo',
       cb
     );
   });
